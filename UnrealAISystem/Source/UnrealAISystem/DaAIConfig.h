1ea24aae5177af5ba936ee9476ad745d9eab3f70
MiloPernemarkDEV <milo.pernemark@student.forsbergsskola.se>
2026-05-04 09:58:10 +0200

    Add Unreal project and AI data asset class

---
+++ UnrealAISystem/Source/UnrealAISystem/DaAIConfig.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "DaAIConfig.generated.h"
+
+class UAISenseConfig;
+class UAISense;
+
+USTRUCT(BlueprintType)
+struct UNREALAISYSTEM_API FTaggedBehaviorTree
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTag Tag;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UBehaviorTree* BehaviorTree = nullptr; 
+};
+
+UCLASS()
+class UNREALAISYSTEM_API UDaAIConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BaseLogic")
+	UBehaviorTree* BaseBehavior = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "StateLogic")
+	TArray<FTaggedBehaviorTree> DynamicBehaviors;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Perception")
+	TArray<TObjectPtr<UAISenseConfig>> SenseConfigs;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Perception")
+	TSubclassOf<UAISense> DominantSense;
+};
